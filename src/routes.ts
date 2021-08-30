c70f961970d6a94efd3c232c293eaf6d7154a8a3
Breno Rodrigues <brenocartoon@hotmail.com>
2021-08-30 20:24:58 -0300

    Adicionar novas rotas

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,6 @@
 import { Router } from "express";
+import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
+import { CreateComplimentsController } from "./controllers/CreateComplimentsController";
 import { CreateTagController } from "./controllers/CreateTagController";
 import { CreateUserControler } from "./controllers/CreateUserController";
 import { ensureAdmin } from "./middlewares/ensureAdmin";
@@ -8,8 +10,12 @@ const router = Router();
 
 const createUserControler = new CreateUserControler();
 const createTagController = new CreateTagController();
+const authenticateUserController = new AuthenticateUserController();
+const createComplimentsController = new CreateComplimentsController();
 
 router.post("/users", createUserControler.handle);
 router.post("/tags", ensureAdmin, createTagController.handle);
+router.post("/login", authenticateUserController.handle);
+router.post("/compliments", createComplimentsController.handle);
 
 export { router };
