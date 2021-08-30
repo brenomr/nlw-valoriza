e0a2f78e3d3653441f72fab4115153e7fa790e23
Breno Rodrigues <brenocartoon@hotmail.com>
2021-08-30 18:06:33 -0300

    Adicionar novo middleware a rota e adicionar

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,15 @@
 import { Router } from "express";
+import { CreateTagController } from "./controllers/CreateTagController";
 import { CreateUserControler } from "./controllers/CreateUserController";
+import { ensureAdmin } from "./middlewares/ensureAdmin";
 
 
 const router = Router();
 
 const createUserControler = new CreateUserControler();
+const createTagController = new CreateTagController();
 
 router.post("/users", createUserControler.handle);
+router.post("/tags", ensureAdmin, createTagController.handle);
 
 export { router };
